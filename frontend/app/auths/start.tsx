32b44a30c30496f551015320794b3d9a15d60f19
Wix-Code <ogbonna428alex@gmail.com>
2025-04-01 12:58:15 +0100

    push

---
+++ frontend/app/auths/start.tsx
@@ -0,0 +1,14 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const start = () => {
+  return (
+    <ScrollView>
+      <View>
+        <Text>start</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+export default start
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,68 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <ScrollView contentContainerStyle={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={styles.contain}>
+        <Image source={require("../assets/images/bag.png")} />
+        <Text style={styles.shopee}>Shopee</Text>
+        <Text style={styles.log}>Beautiful eCommerce UI Kit for your online store</Text>
+      </View>
+      <View style={styles.con}>
+        <TouchableOpacity style={styles.btn}><Text style={styles.text}>Let's get started</Text></TouchableOpacity>
+        <View>
+          <Text style={styles.acc}>I already have an account</Text>
+          <Image source={require("../assets/images/bag.png")}/>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    gap: 60,
+    alignItems: "center",
+  },
+  contain: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  shopee: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  log: {
+    fontSize: 19,
+    textAlign: "center",
+    fontWeight: "normal",
+    color: "#202020"
+  },
+  btn: {
+    backgroundColor: "#004CFF",
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 14
+  },
+  acc: {
+    fontSize: 15,
+    fontWeight: "normal",
+    color: "#202020",
+    textAlign: "center",
+    marginTop: 20
+  },
+  con: {
+    width: "100%",
+  },
+  text: {
+    color: "#F3F3F3",
+    fontSize: 18,
+  }
+});
+
